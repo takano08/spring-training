89b250e4d9e70242d652327eff4e09c9f0f8a465
takano <ri1saxxminny83xx@gmail.com>
2020-07-22 11:14:23 +0900

    singleton

--- Review/src/service/MemberServiceImpl.java
+++ Review/src/service/MemberServiceImpl.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 import entity.Member;
 
 public class MemberServiceImpl implements MemberService {
+	
+	private static MemberServiceImpl singleton = new MemberServiceImpl();
+	
+	private MemberServiceImpl() {};
+	
+	public static MemberServiceImpl getInstance() {
+		return singleton;
+		
+	}
 
 	@Override
 	public String greet(int i) {
